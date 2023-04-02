a7957616c99b70735102cf96b0a48d20cab3ad89
nikita-kasinski <kasinski.nikita@gmail.com>
2023-04-02 17:52:32 +0300

    declare getPasswordHash and getRole callback functions

--- include/model.h
+++ include/model.h
@@ -7,9 +7,15 @@ class Model
 private:
     std::string dbFileName;
     sqlite3 *db = nullptr;
-    
-    // invoked by sqlite during existingLogin method,
+
+    // invoked by sqlite during existingLogin method
     static int existingLogin_Callback(void* optionalArgument, int numberOfColumns, char** data, char** headers);
+
+    // invoked by sqlite during getPasswordHash method
+    static int getPasswordHash_Callback(void* optional, int numberOfColumns, char** data, char** headers);
+
+    //invoked by sqlite during getRole method
+    static int getRole_Callback(void* optional, int numberOfColumns, char** data, char** headers);
 public:
     Model(const std::string& dbFileName);
 
