7caaeb108a6724cfd0c128b44e7db0e26b018bed
nikita-kasinski <kasinski.nikita@gmail.com>
2023-04-02 15:52:08 +0300

    add test for validResponseAuth

--- test/db_test.cc
+++ test/db_test.cc
@@ -12,3 +12,18 @@ TEST(TestConnection, TestNegative)
     Controller ctrl;
     EXPECT_FALSE(ctrl.connectDB("RecordStoreee.db"));
 }
+
+TEST(TestResponceAuth, TestSignInOrSignUp)
+{
+    Controller ctrl;
+    EXPECT_TRUE(ctrl.validResponseAuth("sign-in"));
+    EXPECT_TRUE(ctrl.validResponseAuth("sign-up"));
+}
+
+TEST(TestResponceAuth, TestOther)
+{
+    Controller ctrl;
+    EXPECT_FALSE(ctrl.validResponseAuth("Sign-in"));
+    EXPECT_FALSE(ctrl.validResponseAuth("sign in"));
+    EXPECT_FALSE(ctrl.validResponseAuth("sign off"));
+}
