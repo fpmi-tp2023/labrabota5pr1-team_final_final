7a0fe312168b1808b41d6592e73d2f21da4af015
nikita-kasinski <kasinski.nikita@gmail.com>
2023-04-02 17:24:25 +0300

    declare existingLogin, getPasswordHash and getRole methods in model

--- include/model.h
+++ include/model.h
@@ -8,7 +8,19 @@ private:
     std::string dbFileName;
     sqlite3 *db = nullptr;
 public:
-    Model(const std::string&);
+    Model(const std::string& dbFileName);
+
+    //connects db by dbFileName
     bool connectDB();
+
+    //checks if login exists in db
+    bool existingLogin(const std::string& login) const;
+
+    //gets passwordHash by login
+    std::string getPasswordHash(const std::string& login) const;
+
+    //gets role by login
+    std::string getRole(const std::string& login) const;
+    
     ~Model();
 };
