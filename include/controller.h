078cf25b589f993e1844191bbbf4f6e6a33bb977
nikita-kasinski <kasinski.nikita@gmail.com>
2023-04-02 11:11:39 +0300

    declare destructor and add Model field

--- include/controller.h
+++ include/controller.h
@@ -1,11 +1,14 @@
 #pragma once
 #include <string>
+#include "model.h"
 
 class Controller
 {
 private:
     std::string dbName;
+    Model *dbModel;
 public:
     Controller();
     bool connectDB(const std::string& dbName);
+    ~Controller();
 };
