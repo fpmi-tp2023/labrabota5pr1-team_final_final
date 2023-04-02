8956c67b63fdd5e7501166295d1faad79eb31f6d
nikita-kasinski <kasinski.nikita@gmail.com>
2023-04-02 11:54:43 +0300

    define connectDB and destructor for model

--- src/model.cpp
+++ src/model.cpp
@@ -1,6 +1,25 @@
+#include <iostream>
 #include "model.h"
 
-Model::Model(const std::string& dbFileName)
+Model::Model(const std::string &dbFileName)
 {
     this->dbFileName = dbFileName;
+}
+
+bool Model::connectDB()
+{
+    int openResult;
+    openResult = sqlite3_open(dbFileName.c_str(), &db);
+    if (openResult)
+    {
+        std::cerr << sqlite3_errmsg(db);
+        return false;
+    }
+    return true;
+}
+
+Model::~Model()
+{
+    sqlite3_close(db);
+    db = nullptr;
 }
