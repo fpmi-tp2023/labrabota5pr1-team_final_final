47ac49a7c549a831fec9b739cc999260a896ebe5
nikita-kasinski <kasinski.nikita@gmail.com>
2023-04-02 11:53:09 +0300

    define connectDB func and destructor

--- include/model.h
+++ include/model.h
@@ -1,10 +1,14 @@
 #pragma once
 #include <string>
+#include <sqlite3.h>
 
 class Model
 {
 private:
     std::string dbFileName;
+    sqlite3 *db = nullptr;
 public:
     Model(const std::string&);
+    bool connectDB();
+    ~Model();
 };
