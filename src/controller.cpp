6b34caf82be58ac7ae25c2461263ca62d6a63b33
nikita-kasinski <kasinski.nikita@gmail.com>
2023-04-02 12:12:20 +0300

    define connectDB in controller

--- src/controller.cpp
+++ src/controller.cpp
@@ -4,6 +4,14 @@ Controller::Controller()
 {
 
 }
+
+bool Controller::connectDB(const std::string& dbName)
+{
+    dbModel = new Model(dbName);
+    bool res = dbModel->connectDB();
+    return res;
+}
+
 Controller::~Controller()
 {
     if (dbModel)
