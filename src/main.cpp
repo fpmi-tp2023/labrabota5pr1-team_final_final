09e369488ef0edfba56721e9fa74402ac4545103
nikita-kasinski <kasinski.nikita@gmail.com>
2023-04-02 16:18:43 +0300

    partially implement main program cycle

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,33 @@ int main()
     bool authorized = false;
     const std::string dbFileName = "RecordStore.db";
     Controller ctrl;
+    std::string response;
+    std::string login;
+
     if (!ctrl.connectDB(dbFileName))
     {
         std::cerr << "Something went wrong when opening the database\n";
         return 1;
     }
-    std::string response;
-    while (!ctrl.validResponseAuth(response))
+
+    while (!ctrl.validResponseAuth(response)) // waiting for valid response
     {
-        std::cout << "You are currently unauthorized. Options are sign-in or sign-up";
+        std::cout << "You are currently unauthorized. Enter sign-in or sign-up\n";
         std::cin >> response;
+    }
+
+    if (response == "sign-in")
+    {
+        while(login.empty() || !ctrl.existingLogin(login))
+        {
+            std::cout << "There is no such login";
+            std::cin >> login;
+        }
+
+    }
+    else if (response == "sign-up")
+    {
+
     }
     return 0;
 }
