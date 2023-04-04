f4d5503f922a7801f4a01da1ecc9de4c6747eadc
nikita-kasinski <kasinski.nikita@gmail.com>
2023-04-04 09:03:41 +0300

    implement sign-up in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,9 +15,9 @@ int main()
 
     while (true) // main authorization cycle
     {
-        std::string response;
-        std::string login;
-        std::string role;
+        std::string response = "";
+        std::string role = "";
+        std::string login = "";
 
         if (!authorized)
         {
@@ -88,8 +88,43 @@ int main()
                 // correct password was entered
                 authorized = true;
             }
-            else if (response == "sign-up")
+            else if (response == "sign-up") // option sign-up
             {
+                std::cout << "Enter your desired login or cancel\n";
+                std::cout << "Login: ";
+                std::cin >> login;
+                while(login != "cancel" && ctrl.existingLogin(login)) // wait for cancel or new login
+                {
+                    std::cout << "Your login is already taken, try another one or enter cancel\n";
+                    std::cout << "Login:";
+                    std::cin >> login;
+                }
+
+                if (login == "cancel") // cancel option
+                {
+                    continue; // return to main authorization cycle;
+                }
+
+                // new login was entered, now asking for password
+                std::string password;
+                std::string confirmedPassword;
+                std::cout << "Enter your password\n";
+                std::cout << "Password: ";
+                std::cin >> password;
+                std::cout << "Confirm: ";
+                std::cin >> confirmedPassword;
+                while(!ctrl.passwordsMatch(password, confirmedPassword)) // wait for passwords to match
+                {
+                    std::cout << "Your password doesn't match. Try again on enter cancel\n";
+                    std::cout << "Password: ";
+                    std::cin >> password;
+                    std::cout << "Confirm: ";
+                    std::cin >> confirmedPassword;
+                }
+
+                // now passwords match
+                ctrl.newLogin(login, password); // add new login
+                authorized = true;
             }
         }
         if (authorized)
